d845f54eb48868c5696e91453b0629d2382b74fe
KHM9 <45921659+KHM9@users.noreply.github.com>
2020-05-12 23:34:30 -0600

    UPDATE :D

--- src/KHM9Plugin/main.php
+++ src/KHM9Plugin/main.php
@@ -10,13 +10,13 @@ use pocketmine\command\Command;
 class main extends PluginBase {
 
 	public function onEnable(){
-		$this->getLogger()->info("Hello World!");
+		$this->getLogger()->info("Plugin Enabled!!");
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		switch($command->getName()){
 			case "KHM9":
-				$sender->sendMessage("Example command output");
+				$sender->sendMessage("WOW!! KHM9 MADE A PLUGIN YOWZERS! https://github.com/KHM9/KHM9Plugin-2.0");
 				return true;
 			default:
 				return false;
@@ -24,6 +24,6 @@ class main extends PluginBase {
 	}
 
 	public function onDisable(){
-		$this->getLogger()->info("Bye!");
+		$this->getLogger()->info("PLUGIN ERR. Please contact the developer! https://github.com/KHM9");
 	}
 }
